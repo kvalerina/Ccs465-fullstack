4ddc4b8065fb41424bc80aebe44bcbef23e4d20b
Karina Aronov <karina.aronov@snhu.edu>
2024-11-14 18:52:50 -0500

    Module 3 completed baseline

--- –force/app_server/controllers/travel.js
+++ –force/app_server/controllers/travel.js
@@ -1,6 +1,9 @@
-/*GET travel view*/
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('data/trips.json', 'utf8'));
+
+/*GET travel page. */
 const travel = (req, res) => {
-    res.render('travel', { title: "Trvalr Getaways"});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
