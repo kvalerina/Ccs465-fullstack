0f5957a9d8351e8bcfa94030c954dd7fc820eb67
Karina Aronov <karina.aronov@snhu.edu>
2024-12-03 16:56:41 -0500

    Module 6 completed baseline

--- –force/app.js
+++ –force/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors'); 
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -29,6 +30,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Enable CORS
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+// wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
--- –force/app_api/controllers/trips.js
+++ –force/app_api/controllers/trips.js
@@ -12,7 +12,7 @@ const tripsList = async(req, res) => {
 
         // Uncomment the following line to show results of the querey
         // on the console
-        console.log(q);
+        //console.log(q);
 
     if(!q)
     { // Database rturned no data
@@ -33,7 +33,7 @@ const tripsFindByCode = async(req, res) => {
 
         // Uncomment the following line to show results of the querey
         // on the console
-        console.log(q);
+        //console.log(q);
 
     if(!q)
     { // Database rturned no data
@@ -48,7 +48,79 @@ const tripsFindByCode = async(req, res) => {
 };
 
 
+// POST: /trips - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+
+const tripsAddTrip = async (req, res) => {
+    const newtrip = new Trip({
+      code: req.body.code,
+      name: req.body.name,
+      length: req.body.length,
+      start: req.body.start,
+      resort: req.body.resort,
+      perPerson: req.body.perPerson,
+      image: req.body.image,
+      description: req.body.description,
+    });
+  
+    const q = await newtrip.save();
+  
+        // Uncomment the following line to show results of operation
+        // on the console
+        console.log(q);
+
+    if (!q) {
+      //Database returned no data
+      return res.status(400).json(err);
+    } else {
+      return res.status(201).json(q);
+    }
+  };
+
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    try {
+      const q = await Model.findOneAndUpdate(
+        { code: req.params.tripCode },
+        {
+          code: req.body.code,
+          name: req.body.name,
+          length: req.body.length,
+          start: req.body.start,
+          resort: req.body.resort,
+          perPerson: req.body.perPerson,
+          image: req.body.image,
+          description: req.body.description,
+        }
+      ).exec();
+  
+      if (!q) {
+        // Database returned no data
+        return res.status(404).json({ message: "Trip not found" });
+      } else {
+        // Return resulting updated trip
+        return res.status(200).json(q);
+      }
+  
+      // Uncomment the following line to show results of operation
+      // console.log(q);
+    } catch (error) {
+      console.error("Error updating trip:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
